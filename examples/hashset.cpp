67287e28f3a0b716ab7e67baa3deb3d16fb77832
Dominik Köppl <dominik.koeppl@uni-dortmund.de>
2018-12-11 04:10:36 +0100

    add example for serialize

--- examples/hashset.cpp
+++ examples/hashset.cpp
@@ -6,27 +6,33 @@
 #include <tudocomp/util/compact_hashset/hash_functions.hpp>
 #include <tudocomp/util/compact_hashset/index_structure/cv_bvs_t.hpp>
 #include <algorithm>
+#include <tudocomp/util/serialization.hpp>
 
 
 
-using set_type = tdc::compact_sparse_hashset::generic_hashset_t< 
+
+using set_type = tdc::compact_sparse_hashset::generic_hashset_t<
     tdc::compact_sparse_hashset::poplar_xorshift_t,
     tdc::compact_sparse_hashset::cv_bvs_t
     >;
 
 int main() {
     // creates a set with capacity zero and bit-width five
-    auto set = set_type(0, 5); 
+    auto set = set_type(0, 5);
     for(int i = 0; i <= 4; ++i) { // can hash keys in the range [0..2**5-1]
          set.lookup_insert(i*i);
     }
-    for(int i = 0; i <= 15; ++i) { 
+    for(int i = 0; i <= 15; ++i) {
         auto ret = set.lookup(i);
         if(ret.found()) {
             std::cout << "Id of node : " << ret.id() << std::endl; // returns the unique ID of the entry. This ID does not change until resizing occurs.
             std::cout << i << " -> " << ret.found() << std::endl; // checks whether set[i] is set
             std::cout << std::endl;
         }
-    } 
+    }
+    std::stringstream ss;
+    tdc::serialize<set_type>::write(ss, set);
+    std::cout << ss.str() << std::endl;
+
 }
 
