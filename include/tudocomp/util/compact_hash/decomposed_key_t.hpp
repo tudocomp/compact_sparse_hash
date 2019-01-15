a396dac78d21efac0ce4b768f0a9039991fe0e46
Marvin Löbel <loebel.marvin@gmail.com>
2019-01-15 14:17:40 +0100

    deduplicate more

--- include/tudocomp/util/compact_hash/decomposed_key_t.hpp
+++ include/tudocomp/util/compact_hash/decomposed_key_t.hpp
@@ -2,7 +2,7 @@
 
 #include <cstdint>
 
-namespace tdc {namespace compact_sparse_hashmap {
+namespace tdc {namespace compact_hash {
     struct decomposed_key_t {
         size_t initial_address;   // initial address of key in table
         uint64_t stored_quotient;   // quotient value stored in table
